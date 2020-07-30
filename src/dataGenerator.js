29439f186a1927ff2c28de640d41805e8909889b
Robert Wilcox <bew@panduit.com>
2020-07-29 23:29:43 -0500

    e

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -20,6 +20,10 @@ const brands = [
   "Lenovo"
 ];
 
+const states = [
+"AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MP","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"
+];
+
 function generateDataSets({ size = 1 }) {
   const dataSets = [];
   const currentYear = +d3.timeFormat("%Y")(new Date());
@@ -44,13 +48,38 @@ function generateDataSets({ size = 1 }) {
     });
   }
   console.log("Created Datasets");
+  console.log(dataSets[0];
   return dataSets;
 }
 
 function readData() {
 	
-	d3.csv("data/daily.csv").then(function(data) {
+	d3.csv("data/covid.csv").then(function(data) {
 	  console.log(data[0]);
+
+	  const dataSets = [];
+	  const currentYear = +d3.timeFormat("%Y")(new Date());
+	  const maxLimitForValue = 2000;
+	  const minLimitForValue = 200;
+	  const maximumModelCount = 10;
+
+	  for (let i = 0; i < size; i++) {
+		dataSets.push({
+		  date: data.date,
+		  dataSet: states
+			.sort(function() {
+			  return Math.random() - 0.5;
+			})
+			.slice(0, maximumModelCount)
+			.map(states => ({
+			  name: data.state,
+			  value: data.positive
+			}))
+		});
+	  }
+  console.log("Created Datasets");
+  return dataSets;
+
 	})
 }
 
