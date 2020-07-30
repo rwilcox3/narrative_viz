bc2ce1d1c9a74e4330a9c83439d7102f5190fcf5
Robert Wilcox <bew@panduit.com>
2020-07-29 19:03:12 -0500

    erqw

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -12,9 +12,8 @@ const brands = [
 ];
 // There is no export here stop saying what is there is not there
  function generateDataSets({ size = 1 } ) {
-  const bobguy = 1;
   const dataSets = [];
-  const currentYear = +d3.timeFormat("%Y")(new Date());
+  const currentYear = d3.timeFormat("%Y")(new Date());
   const maxLimitForValue = 2000;
   const minLimitForValue = 200;
 
