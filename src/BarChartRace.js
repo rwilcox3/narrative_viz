b0961f90f00748afbf0a0d5a2d98900a90d582f3
Robert Wilcox <bew@panduit.com>
2020-07-31 12:27:51 -0500

    qwer

--- src/BarChartRace.js
+++ src/BarChartRace.js
@@ -3,7 +3,7 @@
 function BarChartRace(chartId, extendedSettings) {
   const chartSettings = {
     width: 1000,
-    height: 1000,
+    height: 750,
     padding: 40,
     titlePadding: 5,
     columnPadding: .4,
